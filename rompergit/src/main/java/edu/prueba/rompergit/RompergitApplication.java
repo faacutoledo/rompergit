aae8496870bf512ca611d77fb706e5bee6461944
Facundo Toledo <toledo.facundon@gmail.com>
2022-06-02 19:16:30 -0300

    primer commit

---
+++ rompergit/src/main/java/edu/prueba/rompergit/RompergitApplication.java
@@ -0,0 +1,13 @@
+package edu.prueba.rompergit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RompergitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RompergitApplication.class, args);
+	}
+
+}
---
+++ rompergit/src/test/java/edu/prueba/rompergit/RompergitApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.prueba.rompergit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RompergitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
